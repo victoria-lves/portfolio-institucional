3ae34b83b76aadc059c03212eff5ff6dfe7dfa43
Victoria <0076167@academico.ifmg.edu.br>
2025-12-04 11:23:49 -0300

    php changes

--- conexao.php
+++ conexao.php
@@ -6,10 +6,22 @@
     $dbname = "meu_banco_de_dados"; //nome do bd
 
     //cria um objeto de conexão
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = @new mysqli($servername, $username, $password, $dbname);
 
     //checa a conexão
     if($conn->connect_error){
         die("Falha na conexão: " . $conn->connect_error);
     }
+
+    if ($conn-> connect_error){
+        error_log("Erro na conexão MySQL: " . $conn->connect_error);
+        die("Desculpe, tivemos um problema técnico. Tente novamente mais tarde.");    
+    }
+
+    //define o charset para aceitar acentos (UTF-8)
+    if($conn->set_charset("utf8mb4")){
+        error_log("Erro ao carregar o set de caracteres utf8mb4: " . $conn->error);
+        exit();
+    }
+
 ?>
