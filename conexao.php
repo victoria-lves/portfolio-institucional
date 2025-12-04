e8bbfba61e141fc56bfadb90c1abf9a3297c549a
Victoria <0076167@academico.ifmg.edu.br>
2025-12-04 11:13:10 -0300

    alteracao no nome do banco

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "bdPessoa";
+    $dbname = "meu_banco_de_dados";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
