b4df40d09f16534693109c1db10596c9f9c3798f
Victoria <0076167@academico.ifmg.edu.br>
2025-12-04 11:15:01 -0300

    comentarios

--- conexao.php
+++ conexao.php
@@ -1,11 +1,14 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "meu_banco_de_dados";
+//parâmetros de conexão com BD
+    $servername = "localhost"; //nome ou endereço ip do server
+    $username = "root"; //nome do usuário do bd
+    $password = ""; //senha de acesso ao server do bd
+    $dbname = "meu_banco_de_dados"; //nome do bd
 
+    //cria um objeto de conexão
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    //checa a conexão
     if($conn->connect_error){
         die("Falha na conexão: " . $conn->connect_error);
     }
